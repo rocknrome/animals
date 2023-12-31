17fbbbd33e51f933f55a8e68ef08f39d46446801
Roman Larionov <pcsaveddotcom@gmail.com>
2023-12-31 12:19:07 -0500

    controllers and routes created

---
+++ controllers/animals.js
@@ -0,0 +1,110 @@
+// DEPENDENCIES
+module.exports = {
+    index,
+    newForm,
+    destroy,
+    update,
+    create,
+    edit,
+    show,
+    seed
+}
+
+/**
+ * Route Controllers
+ */
+
+async function index(req, res) {
+    // find all of the animals
+    let animals = await req.model.Animal.find({});
+
+    // render all of the animals to index.ejs
+    res.render("index.ejs", {
+        animals: animals.reverse()
+    });
+}
+
+async function newForm(req, res) {
+    // render the create form
+    res.render("new.ejs");
+}
+
+async function destroy(req, res) {
+    try {
+        // Find an animal and then delete
+        let deletedAnimal = await req.model.Animal.findByIdAndDelete(req.params.id);
+        // redirect back to the index
+        res.redirect("/animals");
+
+    } catch (error) {
+        res.status(500).send("something went wrong when deleting");
+    }
+}
+
+async function update(req, res) {
+    try {
+        // Then find by id and update with the req.body
+        let updatedAnimal = await req.model.Animal.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {
+                new: true
+            }
+        );
+
+        // redirect to the show route with the updated animal
+        res.redirect(`/animals/${updatedAnimal._id}`);
+
+    } catch (error) {
+        res.send("something went wrong in this route");
+    }
+}
+
+async function create(req, res) {
+    try {
+        let newAnimal = await req.model.Animal.create(req.body);
+        res.redirect("/animals");
+
+    } catch (err) {
+        res.send(err);
+    }
+}
+
+async function edit(req, res) {
+    try {
+        // find the animal to edit
+        let foundAnimal = await req.model.Animal.findById(req.params.id);
+        res.render("edit.ejs", {
+            animal: foundAnimal
+        });
+    } catch (error) {
+        res.send("hello from the error");
+    }
+}
+
+async function seed(req, res) {
+    try {
+        // delete everything in the database
+        await req.model.Animal.deleteMany({});
+        // Create data in the database
+        await req.model.Animal.create(
+            req.model.seedData
+        );
+
+        // redirect back to the index
+        res.redirect("/animals");
+
+    } catch (error) {
+        res.send("something went wrong with your seeds");
+    }
+}
+
+async function show(req, res) {
+    // find an animal by _id
+    let foundAnimal = await req.model.Animal.findById(req.params.id); // the request params object
+
+    // render show.ejs with the foundAnimal
+    res.render("show.ejs", {
+        animal: foundAnimal
+    });
+}
